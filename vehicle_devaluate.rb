3d4e2bc8a0d38e24ed81d4bad9d3906408c32632
dmcgann1 <dmcgann1@gmail.com>
2014-06-09 17:25:47 -0400

    Added code to calculate depreciation

--- vehicle_devaluate.rb
+++ vehicle_devaluate.rb
@@ -1,3 +1,19 @@
+puts "Whats your first name: "
+first_name = gets.chomp
+
+puts "Whats your last name: "
+last_name = gets.chomp
+
+puts "Whats your first car model?"
+car_model = gets.chomp
+
+puts "What year did you buy it?"
+year = gets.chomp.to_i
+
+puts "How much did it cost?"
+price = gets.chomp.to_i
+
+value_2018 = price * 0.97 ** 8
 
 
-"___ purchased their first ___ in the year ___ they paided ___ dollars for it and it will be worth ___ dollars in the year 2018"
+puts "#{first_name} #{last_name} purchased their first #{car_model} in the year #{year} they paided #{price} dollars for it and it will be worth #{value_2018.to_i} dollars in the year 2018"
